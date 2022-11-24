2c4af23cfd7c8b168f7a61927c0a76a33e70cb46
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-11-24 14:49:24 +0530

    added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentEntryComponent } from './student-entry/student-entry.component';
+
+const myroute : Routes =[
+
+  {
+    path : "",
+    component : StudentEntryComponent
+  }
+]
 
 @NgModule({
   declarations: [
